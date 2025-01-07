b710cba3b74680d33d389ece14ef30809d0206df
猿人易 <qcjxberin@outlook.com>
2025-01-07 16:43:56 +0800

    添加 SpanHelper 类及大端到小端转换方法
    添加了一个新的命名空间 Pek.DataConvertTool。
    在该命名空间下新增了一个 SpanHelper 类。
    在 SpanHelper 类中添加了一个静态方法 ConvertBigToLittle，
    用于将大端字节序转换为小端字节序。
    ConvertBigToLittle 方法接受一个 Span<Byte> 类型的参数 bytes，
    并通过交换字节顺序实现大端到小端的转换。

---
+++ Pek.DataConvertTool/SpanHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Pek.DataConvertTool;
+
+public class SpanHelper
+{
+    /// <summary>
+    /// 大端转为小端。C#数据格式默认为小端  高位在前，低位在后为大端模式
+    /// </summary>
+    /// <param name="bytes"></param>
+    /// <returns></returns>
+    public static void ConvertBigToLittle(Span<Byte> bytes)
+    {
+        var length = bytes.Length;
+        for (var i = 0; i < length / 2; i++)
+        {
+            var temp = bytes[i];
+            bytes[i] = bytes[length - 1 - i];
+            bytes[length - 1 - i] = temp;
+        }
+    }
+}
